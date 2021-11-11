2df4ec8d6060986f8ba2f00f21c4c0ca4a9daf55
Viktoriia Shynkar <viktoriiashynkar2@gmail.com>
2021-11-11 15:57:08 +0200

    правка2

--- js/task-06.js
+++ js/task-06.js
@@ -5,25 +5,27 @@
 // input.addEventListener("blur", onValidSimbolsInput);
 
 // function onValidSimbolsInput(event) {
-// if (event.currentTarget.value.length === Number(input.dataset.length)) {
-//   event.currentTarget.classList.add("valid");
-//   } else {
-//     event.currentTarget.classList.add("invalid");
+//   function classAddRemove(classAdd, classRemove) {
+//     event.currentTarget.classList.add(classAdd);
+//     event.currentTarget.classList.remove(classRemove);
+//   }
+//   classAddRemove("invalid");
+//   if (event.currentTarget.value.length === Number(input.dataset.length)) {
+//     classAddRemove("valid", "invalid");
 //   }
 // }
-// .........................................................
-
+// .......................................................
 const input = document.querySelector("#validation-input");
 
 input.addEventListener("blur", onValidSimbolsInput);
 
 function onValidSimbolsInput(event) {
-  function classAddRemove(classAdd, classRemove) {
-    event.currentTarget.classList.add(classAdd);
-    event.currentTarget.classList.remove(classRemove);
-  }
   classAddRemove("invalid");
   if (event.currentTarget.value.length === Number(input.dataset.length)) {
     classAddRemove("valid", "invalid");
   }
+}
+function classAddRemove(classAdd, classRemove) {
+  event.currentTarget.classList.add(classAdd);
+  event.currentTarget.classList.remove(classRemove);
 }
