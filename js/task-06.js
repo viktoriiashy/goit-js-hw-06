95170e4fc563327b2005d2027c4c876c3419f36b
Viktoriia Shynkar <viktoriiashynkar2@gmail.com>
2021-11-11 10:41:04 +0200

    правка

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,29 @@
 "use strict";
 
+// const input = document.querySelector("#validation-input");
+
+// input.addEventListener("blur", onValidSimbolsInput);
+
+// function onValidSimbolsInput(event) {
+// if (event.currentTarget.value.length === Number(input.dataset.length)) {
+//   event.currentTarget.classList.add("valid");
+//   } else {
+//     event.currentTarget.classList.add("invalid");
+//   }
+// }
+// .........................................................
+
 const input = document.querySelector("#validation-input");
 
 input.addEventListener("blur", onValidSimbolsInput);
 
 function onValidSimbolsInput(event) {
-  if (event.currentTarget.value.length === 6) {
-    event.currentTarget.classList.add("valid");
-  } else {
-    event.currentTarget.classList.add("invalid");
+  function classAddRemove(classAdd, classRemove) {
+    event.currentTarget.classList.add(classAdd);
+    event.currentTarget.classList.remove(classRemove);
+  }
+  classAddRemove("invalid");
+  if (event.currentTarget.value.length === Number(input.dataset.length)) {
+    classAddRemove("valid", "invalid");
   }
 }
